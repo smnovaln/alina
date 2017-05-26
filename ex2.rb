4becd45549526b0f1931f7922aeb25b45a532a6e
smnovaln <smnovaln1993@gmail.com>
2017-05-27 00:40:23 +0300

    a*h*1/2

---
+++ ex2.rb
@@ -0,0 +1,10 @@
+puts "Enter a - the base of triangle"
+a = gets.chomp
+
+puts "Enter h - Height of the triangle"
+h = gets.chomp
+
+s = a.to_i * h.to_i * 0.5
+
+puts "The area of ​​the triangle is #{s}"
+
